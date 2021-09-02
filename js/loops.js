1b4d4af0245dc342f2484a014f8fd4b099f0afcc
flores-luis <ricardoluis6212@gmail.com>
2021-09-02 07:56:13 -0700

    added more information

--- js/loops.js
+++ js/loops.js
@@ -2,4 +2,19 @@
 
 This program will explain all loops in Javascript with examples
 
+Loops essentially do same things, repeat an action a number of times, but
+difference of how they start and end.
+
+Loops are: for, while, do while, for...in, for...of
 */
+
+
+// For loop
+
+// While loop
+
+// Do While loop
+
+// For...in loop
+
+// For...of loop
