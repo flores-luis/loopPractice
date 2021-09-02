8a461f8a4b476acb7a4566df7a8628d91b4b996a
flores-luis <ricardoluis6212@gmail.com>
2021-09-02 08:08:55 -0700

    for loop

--- js/loops.js
+++ js/loops.js
@@ -8,8 +8,19 @@ difference of how they start and end.
 Loops are: for, while, do while, for...in, for...of
 */
 
-
 // For loop
+console.log("This is a For loop")
+/*
+    for (Initial Expression / Declaration; Condition; Increment Expression) {
+        Statement
+    }
+
+NOTE:
+- i is used as index as a starting point
+*/
+
+    for (let i = 0; i < 10; i++)
+        console.log(i)
 
 // While loop
 
