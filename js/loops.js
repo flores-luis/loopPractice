ddeed2fcfecbe7cc03a2a8e30722dc03b135fc81
flores-luis <ricardoluis6212@gmail.com>
2021-09-03 23:14:19 -0700

    added info

--- js/loops.js
+++ js/loops.js
@@ -8,7 +8,7 @@ difference of how they start and end.
 Loops are: for, while, do while, for...in, for...of
 */
 
-// For loop
+// For loop----------------------------------------------------------------------------------------
 console.log("This is a For loop")
 /*
     for (Initial Expression / Declaration; Condition; Increment Expression) {
@@ -21,7 +21,8 @@ or
     }
 NOTE:
 
-The statement1 is executed first even before executing the looping code. So, this statement is normally used to assign values to variables that will be used inside the loop.
+The statement1 is executed first even before executing the looping code. So, 
+this statement is normally used to assign values to variables that will be used inside the loop.
 The statement2 is the condition to execute the loop.
 The statement3 is executed every time after the looping code is executed.
 - i is used as index as a starting point
@@ -30,7 +31,7 @@ The statement3 is executed every time after the looping code is executed.
     for (let i = 0; i < 10; i++)
         console.log(i)
 
-// While loop
+// While loop----------------------------------------------------------------------------------------
 console.log("This is a While Loop")
 /*
 
@@ -40,6 +41,9 @@ console.log("This is a While Loop")
     }
 
 NOTE: While loop is support by a global variable
+he “while loop” is executed as long as the specified condition is true. 
+Inside the while loop, you should include the statement that will end 
+the loop at some point of time. Otherwise, your loop will never end and your browser may crash.
 */
 
     let num = 0
@@ -48,8 +52,10 @@ NOTE: While loop is support by a global variable
         console.log(num);
         num++;
     }
-// Do While loop
 
-// For...in loop
 
-// For...of loop
+// Do While loop----------------------------------------------------------------------------------------
+
+// For...in loop----------------------------------------------------------------------------------------
+
+// For...of loop----------------------------------------------------------------------------------------
